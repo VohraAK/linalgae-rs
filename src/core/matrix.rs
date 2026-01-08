50bb0930ffae48eaa6acdc03ee58e80b977997a5
VohraAK <abdullah.k.vohra@gmail.com>
2026-01-08 20:27:32 +0500

    added Matrix struct, basic accessors

---
+++ src/core/matrix.rs
@@ -0,0 +1,105 @@
+use std::fmt;
+
+// defining a matrix struct (f64)
+#[derive(Debug)]
+pub struct Matrix
+{
+    rows: usize,
+    cols: usize,
+    data: Vec<f64>
+}
+
+impl Matrix
+{
+    // "default" constructor
+    pub fn new(rows: usize, cols: usize, data: Vec<f64>) -> Result<Self, &'static str>
+    {
+        // check rows and cols
+        if rows <= 0
+        {
+            return Err("Row dim should be positive!");
+        }
+
+        if cols <= 0
+        {
+            return Err("Col dim should be positive!");
+        }
+
+        // check if number of elements equal to (rows x cols)
+        if (rows * cols) != data.len()
+        {
+            return Err("Input vector does not match dimensions!");
+        }
+        
+        Ok(Matrix { rows, cols, data })
+    }
+
+    // "zeros" constructor, returns a zeroed out Matrix
+    pub fn zeroes(rows: usize, cols: usize) -> Result<Self, &'static str>
+    {
+        // check rows and cols
+        if rows <= 0
+        {
+            return Err("Row dim should be positive!");
+        }
+
+        if cols <= 0
+        {
+            return Err("Col dim should be positive!");
+        }
+
+        Ok(Matrix { rows, cols, data: vec![0 as f64; rows * cols] })
+    }
+
+    // dim getters
+    pub fn rows(&self) -> usize { self.rows }
+    pub fn cols(&self) -> usize { self.cols }
+
+    // data accessors implemented as slicers
+    pub fn as_slice(&self) -> &[f64] { &self.data }
+    pub fn as_mut_slice(&mut self) -> &mut[f64] { &mut self.data }
+
+}
+
+
+
+
+
+
+
+
+// impl fmt::Display trait for Matrix
+impl fmt::Display for Matrix
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result 
+    {
+        write!(f, "\n[")?;
+
+        for row in 0..self.rows
+        {
+            write!(f, "[")?;
+
+            for col in 0..self.cols
+            {
+                let idx = row * self.cols + col;
+                write!(f, "{:.2}", self.data[idx])?;
+
+                if col != self.cols - 1
+                {
+                    write!(f, ", ")?;
+                }
+            }
+
+            write!(f, "]")?;
+
+            if row != self.rows - 1
+            {
+                write!(f, ",\n")?;
+            }
+        }
+
+        write!(f, "]")?;
+
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::process;
+use linalgae_rs::core::matrix::Matrix;
+
+fn main()
+{
+    let matrix_1 = Matrix::zeroes(15, 12).unwrap_or_else(|e| 
+        {
+            eprintln!("Error: {e}");
+            process::exit(1);
+        });
+
+    println!("{}", matrix_1);
+    println!("Dim: {} x {}", matrix_1.rows(), matrix_1.cols());
+
+    let matrix_2 = Matrix::new(4, 2, vec![2., 3., 4., 81., -3., 7.44, -1110., 29229.]).unwrap_or_else(|e| 
+        {
+            eprintln!("Error: {e}");
+            process::exit(1);
+        });
+
+    let view = matrix_2.as_slice();
+    println!("\nPrinting elements...");
+
+    for &ele in view { print!("{}, ", ele);}
+
+}
