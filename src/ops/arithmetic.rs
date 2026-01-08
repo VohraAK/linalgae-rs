0a349cc214d4e409c813d76bc54dc2ad4312d4e8
VohraAK <abdullah.k.vohra@gmail.com>
2026-01-08 23:30:17 +0500

    added scalar mul and div

--- src/ops/arithmetic.rs
+++ src/ops/arithmetic.rs
@@ -2,7 +2,6 @@ use std::ops;
 use crate::core::matrix::Matrix;
 
 
-
 // ----------Operator Overloading----------//
 
 // addition
@@ -52,5 +51,53 @@ impl ops::Sub<&Matrix> for &Matrix
 
         Matrix::new(lhs_rows, lhs_cols, result).expect("Matrix::Sub: Result has incorrect dimensions!")
         
+    }
+}
+
+// scalar mul (Matrix * scalar)
+impl ops::Mul<f64> for &Matrix
+{
+    type Output = Matrix;
+
+    fn mul(self, rhs: f64) -> Self::Output 
+    {
+        let lhs_rows = self.rows();
+        let lhs_cols = self.cols();
+
+        let result = self.as_slice().iter().map(|a|  a * rhs).collect();
+
+        Matrix::new(lhs_rows, lhs_cols, result).expect("Matrix::Mul: Result has incorrect dimensions!")
+        
+    }
+}
+
+// scalar mul (scalar * Matrix)
+impl ops::Mul<&Matrix> for f64
+{
+    type Output = Matrix;
+
+    fn mul(self, rhs: &Matrix) -> Self::Output { rhs * self }
+}
+
+// scalar div (Matrix / scalar)
+impl ops::Div<f64> for &Matrix
+{
+    type Output = Matrix;
+
+    fn div(self, rhs: f64) -> Self::Output 
+    {
+        // TODO: epsilon
+        // check 0-divisor
+        if rhs == (0 as f64)
+        {
+            panic!("Matrix::Div: Cannot divide by zero!");
+        }
+
+        let lhs_rows = self.rows();
+        let lhs_cols = self.cols();
+
+        let result = self.as_slice().iter().map(|a| a / rhs).collect();
+
+        Matrix::new(lhs_rows, lhs_cols, result).expect("Matrix::Div: Result has incorrect dimensions!") 
     }
 }
